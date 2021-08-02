bd4eec06f542452e4014a8c0e1910528c5d9af96
Miki-mi <blackshadow1617@gmail.com>
2021-08-02 18:13:16 +0700

    change db connect

--- database/db.php
+++ database/db.php
@@ -1,18 +1,12 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+$db_params = parse_ini_file( dirname(__FILE__).'/db_params.ini', false );
 
-$dotenv = new Dotenv\Dotenv(__DIR__, './../../../inc/dbinfo.env');
-$dotenv->load();
 
-$DB_SERVER = env(DB_SERVER);
-$DB_USERNAME = env(DB_USERNAME);
-$DB_PASSWORD = env(DB_PASSWORD);
-$DB_DATABASE = env(DB_DATABASE);
 
 try {
     //$conn = new mysqli('localhost', 'root', '', 'guestbook');
-	$conn = new mysqli($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
+	$conn = new mysqli($db_params['host'], $db_params['user'], $db_params['password'], $db_params['dbname']);
 } catch (Exception $e) {
 	echo 'Connection failed';
 }
