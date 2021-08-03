31975fe0f24b520d24d54af318109e95b8fc40b5
Miki-mi <blackshadow1617@gmail.com>
2021-08-03 08:54:23 +0700

    update code db

--- database/db.php
+++ database/db.php
@@ -2,8 +2,6 @@
 
 $db_params = parse_ini_file( dirname(__FILE__).'/db_params.ini', false );
 
-
-
 try {
     //$conn = new mysqli('localhost', 'root', '', 'guestbook');
 	$conn = new mysqli($db_params['host'], $db_params['user'], $db_params['password'], $db_params['dbname']);
