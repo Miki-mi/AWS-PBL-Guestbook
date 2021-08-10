269d001609e66c52e3b66e46cda9b488bf2ef760
Michelle <71686934+Miki-mi@users.noreply.github.com>
2021-08-10 18:46:31 +0700

    Update guestbook.sql

--- database/guestbook.sql
+++ database/guestbook.sql
@@ -20,8 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `guestbook`
 --
-CREATE DATABASE IF NOT EXISTS guestbook;
-USE guestbook;
+USE guestbookDB;
 -- --------------------------------------------------------
 
 --
